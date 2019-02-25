c689c3b15c2cf0e7fe64c7177f986fdaf4a38f72
Tyler Valley <tvalley93@gmail.com>
2019-02-25 16:35:27 -0500

    Fix environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -12,5 +12,5 @@ ActionMailer::Base smtp_settings = {
   :user_name => ENV{'SENDGRID_USERNAME'},
   :password => ENV{'SENDGRID_PASSWORD'},
   :domain => 'heroku.com',
-  :enable_starttls_auto -> true
+  :enable_starttls_auto => true
 }
